edbe504f067b8d88f54d4beeab211145144fc2ba
Laurent-Ch <chamouleau.laurent@gmail.com>
2022-01-26 20:21:29 +0100

    pagelist isn't complete but every coded part works

--- src/PageList.js
+++ src/PageList.js
@@ -1,10 +1,11 @@
 import { rawgKey } from "../rawgKey";
 
-const PageList = (argument = '') => {
+const PageList = () => {
+  let searchTerm;
   let numberOfLoadedPages = 1;
-  const preparePage = () => {
-    const cleanedArgument = argument.replace(/\s+/g, "-");
-    const displayResults = (articles, isShowMore) => {
+  let resultsContainer;
+
+  const displayResults = (articles, isShowMore = false) => {
     let arrayImg = ['' ,'<i class="fab fa-windows" style="font-size:30px"></i>', '<i class="fab fa-playstation" style="font-size:30px"></i>', '<i class="fab fa-xbox" style="font-size:30px"></i>', '<i class="fab fa-app-store-ios" style="font-size:30px"></i>', '<i class="fab fa-apple" style="font-size:30px"></i>', '<i class="fab fa-linux" style="font-size:30px"></i>', '<i class="fab fa-nintendo-switch" style="font-size:30px"></i>', '<i class="fab fa-android" style="font-size:30px"></i>'];
     
     const resultsContent = articles.results.map((article) => (`
@@ -21,57 +22,65 @@ const PageList = (argument = '') => {
           <div>${article.parent_platforms.map(e => arrayImg[e.platform.id]).join(' ')}</div>
         </article>
     `));
-    const resultsContainer = document.querySelector(".page-list .articles");
     
     if (isShowMore == false) {
       resultsContainer.innerHTML = resultsContent.join("\n");
     } else {
       resultsContainer.innerHTML += resultsContent.join("\n");
     }
+  };
 
-    let btnShowMore = document.querySelector('.show-more-button');
+  const fetchList = (url, argument, isShowMore = false) => {
+    const finalURL = argument ? `${url}&search=${argument}` : url;
+    fetch(finalURL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        displayResults(responseData, isShowMore);
+      });
+  };
+  
+  const preparePage = () => {
+    resultsContainer = document.querySelector(".page-list .articles");
+
+    // Recovering the user search and transfering it when necessary
+    let searchBar = document.querySelector('.searchbar');
+    searchBar.addEventListener('keypress', e => {
+      if (e.key == 'Enter') {
+        e.preventDefault();
+        numberOfLoadedPages = 1;
+        resultsContainer.innerHTML = '';
+        searchTerm = searchBar.value.trim().replace(/\s+/g, "-");
+        fetchList(`https://api.rawg.io/api/games?key=${rawgKey}&page_size=9`, searchTerm);
+      }
+    });
+  
+    fetchList(`https://api.rawg.io/api/games?key=${rawgKey}&page_size=9`);
+  };
+
+  const handleShowBtn = () => {
+    const btnShowMore = document.querySelector('.show-more-button');
     btnShowMore.addEventListener('click', () => {
       numberOfLoadedPages++;
-      console.log(numberOfLoadedPages);
-      fetchList(articles.next, cleanedArgument, true);
-    }, { once: true });
-
-    if(numberOfLoadedPages >= 3 ){
-      btnShowMore.style.display='none';
-    }
-  };
-
-    const fetchList = (url, argument, isShowMore = false) => {
-      const finalURL = argument ? `${url}&search=${argument}` : url;
-      console.log(`finalURL: ${finalURL}`);
-      fetch(finalURL)
-        .then((response) => response.json())
-        .then((responseData) => {
-          displayResults(responseData, isShowMore);
-        });
-    };
-
-    fetchList(`https://api.rawg.io/api/games?key=${rawgKey}&page_size=9`, cleanedArgument);
-  };
+      if(numberOfLoadedPages >= 3 ){
+        btnShowMore.style.display='none';
+      }
+      fetchList(`https://api.rawg.io/api/games?key=${rawgKey}&page_size=9&page=${numberOfLoadedPages}`, searchTerm, true);
+    });
+  }
 
   const render = () => {
-    let showMore = `
-    <div class='btn-show-more-container'>  
-      <button type='button' class='show-more-button'>Show More</button>
-    </div>
-    `
-    
     pageContent.innerHTML = `
       <section class="page-list">
         <div class="articles">...loading</div>
       </section>
+      <div class='btn-show-more-container'>  
+        <button type='button' class='show-more-button'>Show More</button>
+      </div>
     `
-    if (numberOfLoadedPages <= 3) {
-      pageContent.innerHTML += showMore;
-    };
-
+    handleShowBtn();
     preparePage();
   };
+
   render();
 };
 
