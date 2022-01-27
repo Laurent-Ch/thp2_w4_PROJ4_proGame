76ff0cfceabe61dd48c59e9fc41bc8c1ad89cd5f
Laurent-Ch <chamouleau.laurent@gmail.com>
2022-01-27 11:39:21 +0100

    main page pitch linked to home function, pagedetail background img installed

--- src/PageDetail.js
+++ src/PageDetail.js
@@ -2,12 +2,16 @@ import { rawgKey } from "../rawgKey";
 
 // The search is done by id, not by name. Makes things way simpler.
 const PageDetail = (argument) => {
+  let intro = document.querySelector('#mainpage-pitch');
+  intro.innerHTML = '';
+  
   let baseAddress = 'https://api.rawg.io/api/games'; 
   const preparePage = () => {
 
     const displayGame = (gameData) => {
-      const { name, released, description } = gameData;
+      const { background_image, name, released, description } = gameData;
       const articleDOM = document.querySelector(".page-detail .article");
+      articleDOM.querySelector(".detail-image").src = background_image;
       articleDOM.querySelector("h1.title").innerHTML = name;
       articleDOM.querySelector("p.release-date span").innerHTML = released;
       articleDOM.querySelector("p.description").innerHTML = description;
@@ -28,6 +32,7 @@ const PageDetail = (argument) => {
     pageContent.innerHTML = `
       <section class="page-detail">
         <div class="article">
+          <img class="detail-image" src"" alt="game-image"/>
           <h1 class="title"></h1>
           <p class="release-date">Release date : <span></span></p>
           <p class="description"></p>
